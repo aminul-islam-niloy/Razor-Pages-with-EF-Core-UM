886846b9b576baaba98c49dd5b9a05dd05b7cc06
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-02-16 21:15:28 +0600

    Enrollment and Overposting on Student Create and Details

--- EF_with_Razor/Pages/Students/Details.cshtml.cs
+++ EF_with_Razor/Pages/Students/Details.cshtml.cs
@@ -28,14 +28,19 @@ namespace EF_with_Razor.Pages.Students
                 return NotFound();
             }
 
-            var student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
-            if (student == null)
+         Student = await _context.Students
+        .Include(s => s.Enrollments)
+        .ThenInclude(e => e.Course)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
             {
                 return NotFound();
             }
             else 
             {
-                Student = student;
+                Student = Student;
             }
             return Page();
         }
